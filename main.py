9cb9fcc6b56820e815a595ca8d859207369c9c2f
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2022-10-21 23:14:59 +0000

    Created input file check if user wants new output file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+import os
 
 def search_dictionary(word, dictionary):
     list = []
@@ -23,14 +23,24 @@ with open('transcripts.json', 'r', encoding='cp850') as jsonFile:
 
     #print(data)
 
-with open('output.txt', 'a') as file:
-    try:
-        while True:
-            try:
-                input
-                file.write(search_dictionary(input("\ninput text: "), data))
-            except:
-                pass
+with open('input.txt') as f:
+    inputList = f.readlines() # read all lines into a list
+    inputList = [line.rstrip() for line in inputList] # remove '\n' at the end of each line otherwise it will be searched for with \n at the end
+    print(inputList)# print the list to see if it worked
 
-    finally:
-        file.close()
+#check if user wants new output file
+if input("Do you want to create a new output file? (y/n): ") == "y":
+    os.remove("output.txt")
+
+
+with open('output.txt', 'a') as file:
+    count = 0 # count the number of lines in the input file for i didnt work
+    while count < len(inputList):
+        try:# if the search returns nothing it will throw an error and the program will continue
+            file.write(search_dictionary(inputList[count], data))
+            count += 1
+        except:
+            pass
+
+file.close()
+f.close()
